3946210068fa2983832213162e2664341316fc4e
elirweb <elirweb@gmail.com>
2016-02-20 01:01:26 -0200

    primeiro commit no vs 2013

---
+++ ImovelWeb/ImovelWeb.Startup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImovelWeb.Startup.MvcApplication" Language="C#" %>
---
+++ ImovelWeb/ImovelWeb.Startup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImovelWeb.Startup.Startup))]
+namespace ImovelWeb.Startup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
