a2556de225bf6edd7593069fd620b83df203ee5a
elirweb <elirweb@gmail.com>
2016-02-21 01:33:15 -0300

    commit sexto

---
+++ ImovelWeb/ImovelWeb.Startup/Areas/Administrador/Controllers/AdminController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ImovelWeb.Startup.Areas.Administrador.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Administrador/Admin
+       public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(FormCollection model)
+        {
+            using (RepositoryImovel.RepositoryCorretor corretor = new RepositoryImovel.RepositoryCorretor()) {
+
+                if (corretor.Authenticar(model["Login"], model["Senha"]))
+                {
+                    FormsAuthentication.SetAuthCookie(model["Login"], false);
+                    return RedirectToAction("/Home");
+                }
+                else
+                {
+                    ViewBag.mensagem = "Usuario ou Senha não confere";
+                    return View();
+                }
+            }
+            
+            
+        }
+
+        public static void Deslogar() {
+
+            FormsAuthentication.SignOut();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadcorretor(DDDImovel.ValuesObject.Model.Corretor corretor)
+        {
+            var msg = string.Empty;
+            var comitar = false;
+            EmailCorretor sendecorretor = new EmailCorretor();
+            if (ModelState.IsValid)
+            {
+                using (RepositoryImovel.RepositoryCorretor scorretor = new RepositoryImovel.RepositoryCorretor()) {
+                    scorretor.Inserir(corretor);
+                    //sendecorretor.EnviarEmailCorretor(corretor.Email);
+                    RepositoryImovel.RepositoryRegistro registro = new RepositoryImovel.RepositoryRegistro();
+
+                    // inserir novo registro
+                    registro.NovoCorretor(corretor);
+                    
+            };
+            
+            comitar = true;
+
+        }
+
+        if (comitar)
+            msg = "Cadastro efetuado com sucesso";
+        else
+            msg = "Erro! no cadastro";
+    
+            return Json(msg, JsonRequestBehavior.AllowGet);
+
+
+        }
+        
+        [Authorize]
+        public ActionResult Home() {
+            
+            return View();
+        }
+        [Authorize]
+        public ActionResult Foto() {
+            
+            
+            RepositoryImovel.RepositoryEmpreendimento emp = new RepositoryImovel.RepositoryEmpreendimento();
+            ViewData["EmpreendimentoID"] = new SelectList(emp.ObterTodos(), "EmpreendimentoID", "Nome");
+         
+            return View();
+        }
+
+
+
+        [ValidateAntiForgeryToken]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Foto(HttpPostedFileBase file,Foto foto)
+        {
+            var msg = "";
+            if (ModelState.IsValid) {
+                using (RepositoryImovel.RepositorioFoto fot = new RepositoryImovel.RepositorioFoto()) {
+                    fot.Inserir(foto);
+                    fot.Foto(file, foto.NomeFoto);
+                    ViewBag.msg = "Dados cadastrados com sucesso";
+                    
+                }
+            }
+            
+            RepositoryImovel.RepositoryEmpreendimento emp = new RepositoryImovel.RepositoryEmpreendimento();
+            ViewData["EmpreendimentoID"] = new SelectList(emp.ObterTodos(), "EmpreendimentoID", "Nome");
+         
+            return View();
+        }
+
+        
+        [Authorize]
+        public ActionResult Imovel() {
+            RepositoryImovel.RepositoryEmpreendimento emp = new RepositoryImovel.RepositoryEmpreendimento();
+            ViewData["EmpreendimentoID"] = new SelectList(emp.ObterTodos(), "EmpreendimentoID", "Nome");
+         
+            RepositoryImovel.RepositoryPorcentagem porc = new RepositoryImovel.RepositoryPorcentagem();
+            ViewData["PorcentagemID"] = new SelectList(porc.ObterTodos(), "PorcentagemID", "Desconto");
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Imovel(Imovel imovel) {
+            var msg = "";
+            if (ModelState.IsValid) {
+                using (RepositoryImovel.RepositoryImobiliario imo = new RepositoryImovel.RepositoryImobiliario()) {
+                    imo.Inserir(imovel);
+                    msg = "Cadastrado com sucesso";
+                }
+            }
+
+
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+
+        
+        [Authorize]
+        public ActionResult Empreendimento() {
+
+            RepositoryImovel.RepositoryCorretor corretor = new RepositoryImovel.RepositoryCorretor();
+            var  idcorretor =  corretor.Localizar(c => c.Email.Equals(User.Identity.Name));
+            foreach(var item in idcorretor){
+                ViewBag.IDCorretor = item.CorretorID;
+           
+            }
+
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Empreendimento(Empreendimento model) {
+            var msg = "";
+            if (ModelState.IsValid) {
+
+                using (RepositoryImovel.RepositoryEmpreendimento empreendimento = new RepositoryImovel.RepositoryEmpreendimento()) {
+                    empreendimento.Inserir(model);
+                    msg = "Cadastrado com sucesso";
+                }
+            }
+
+
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [Authorize]
+        public ActionResult Porcentagem() {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Porcentagem(Porcentagem porcentagem)
+        {
+            var msg = "";
+            if (ModelState.IsValid) {
+                using (RepositoryImovel.RepositoryPorcentagem porc = new RepositoryImovel.RepositoryPorcentagem())
+                {
+                    var proc = porc.Localizar(x => x.Desconto.Equals(porcentagem));
+                    if (proc != null)
+                    {
+                        msg = "Desconto cadastrado no sistema";
+                    }
+                    else
+                    {
+                        porc.Inserir(porcentagem);
+                        msg = "Porcentagem inserido com sucesso";
+                    }
+                }
+            }
+            return Json(msg,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult EsqueceuEmail(string email,string senha) {
+            EmailCorretorEsqueceuSenha sendecorretor = new EmailCorretorEsqueceuSenha();
+            sendecorretor.EnviarEmailCorretor(email,senha);        
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+    }
+}
---
+++ ImovelWeb/ImovelWeb.Startup/Areas/Content/script/script_js.js
@@ -0,0 +1,342 @@
+﻿var ValidateLogar = function () {
+        if ($("#Login").val() == "") {
+            Mensagem("Favor informar o Login");
+            document.getElementById("Login").focus();
+            return false;
+        } else if ($("#Senha").val() == "") {
+            Mensagem("Favor informar a Senha");
+            document.getElementById("Senha").focus();
+            return false;
+        }
+        else {
+
+            $("#mensagem").css('display', 'none');
+            $("#mensagem").html();
+            document.getElementById("FormLogar").submit();
+        }
+   
+};
+var Mensagem = function (mensagem) {
+        $("#mensagem").css('display', 'block');
+        $("#mensagem").html(mensagem);
+     
+};
+
+
+
+var AbrirFormCorretorCadastro = function () {
+    $("#MsgGeral").css('display', 'block').animate({ opacity: 0.10 }).slideUp;
+    $("#CadCorretor").css('display', 'block').fadeIn(500);
+};
+
+var Closed = function () {
+    $("#MsgGeral").css('display', 'none');
+    $("#CadCorretor").css('display', 'none');
+    $("#EmailCadastro").css('display', 'none');
+
+};
+
+var AbrirEmailCadastro = function () {
+    $("#MsgGeral").css('display', 'block').animate({ opacity: 0.10 }).slideUp;
+    $("#EmailCadastro").css('display', 'block').fadeIn(500);
+    document.getElementById("esqueciemail").value = "";
+};
+
+function Formata(campo, tammax, teclapres, decimal) {
+    var tecla = teclapres.keyCode;
+    vr = Limpar(campo.value, "0123456789");
+    tam = vr.length;
+    dec = decimal
+
+    if (tam < tammax && tecla != 8) { tam = vr.length + 1; }
+
+    if (tecla == 8)
+    { tam = tam - 1; }
+
+    if (tecla == 8 || tecla >= 48 && tecla <= 57 || tecla >= 96 && tecla <= 105) {
+
+        if (tam <= dec)
+        { campo.value = vr; }
+
+        if ((tam > dec) && (tam <= 5)) {
+            campo.value = vr.substr(0, tam - 2) + "," + vr.substr(tam - dec, tam);
+        }
+        if ((tam >= 6) && (tam <= 8)) {
+            campo.value = vr.substr(0, tam - 5) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - dec, tam);
+        }
+        if ((tam >= 9) && (tam <= 11)) {
+            campo.value = vr.substr(0, tam - 8) + "." + vr.substr(tam - 8, 3) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - dec, tam);
+        }
+        if ((tam >= 12) && (tam <= 14)) {
+            campo.value = vr.substr(0, tam - 11) + "." + vr.substr(tam - 11, 3) + "." + vr.substr(tam - 8, 3) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - dec, tam);
+        }
+        if ((tam >= 15) && (tam <= 17)) {
+            campo.value = vr.substr(0, tam - 14) + "." + vr.substr(tam - 14, 3) + "." + vr.substr(tam - 11, 3) + "." + vr.substr(tam - 8, 3) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - 2, tam);
+        }
+    }
+
+}
+
+function Limpar(valor, validos) {
+    // retira caracteres invalidos da string
+    var result = "";
+    var aux;
+    for (var i = 0; i < valor.length; i++) {
+        aux = validos.indexOf(valor.substring(i, i + 1));
+        if (aux >= 0) {
+            result += aux;
+        }
+    }
+    return result;
+}
+
+function SomenteNumero(e) {
+    var tecla = (window.event) ? event.keyCode : e.which;
+    if ((tecla > 47 && tecla < 58)) return true;
+    else {
+        if (tecla == 8 || tecla == 0) return true;
+        else return false;
+    }
+}
+
+var SalvarCorretor = function () {
+    var dados = $("#FormCorretor").serialize();
+    $.ajax({
+        url: 'Admin/Cadcorretor',
+        type: 'post',
+        datatype: 'json',
+        cache: false,
+        method: 'post',
+        data: dados,
+        success: function (data, textSaudacoes) {
+            if (data.indexOf("Erro!") == 0) {
+                $("#resposta").text(data);
+             } else {
+                $("#resposta").text(data);
+                document.getElementById("Matricula").value = "";
+                document.getElementById("NomeCorretor").value = "";
+                document.getElementById("Login").value = "";
+                document.getElementById("senhacorretor").value = "";
+                document.getElementById("Endereco").value = "";
+                document.getElementById("Email").value = "";
+                document.getElementById("Cidade").value = "";
+                document.getElementById("Telefone").value = "";
+                document.getElementById("logincorretor").value = "";
+                document.getElementById("Cidade").value = "";
+                document.getElementById("Telefone").value = "";
+                document.getElementById("resenha").value = "";
+                $('#sexo').find('option:first').attr('selected', 'selected');
+                $('#Estado').find('option:first').attr('selected', 'selected');
+
+            }
+        },
+        error: function (data, textSaudacoes) {
+            $("#resposta").text(data);
+        }
+    });
+};
+
+var EsqueceuEmailCorretor = function () {
+    
+    if (document.getElementById("esqueciemail").value != "") {
+        var enviaremail = false;
+        var dados = $("#FormEmail").serialize();
+        $.ajax({
+            url: 'http://localhost:51744/api/corretor/?valor=' + document.getElementById("esqueciemail").value,
+            type: 'get',
+            datatype: 'json',
+            cache: false,
+            method: 'post',
+            befoseSend: function () {
+                alert('Aguarde');
+            },
+            success: function (data, textSaudacoes) {
+                $.each(data, function (i, obj) {
+                    if (obj.email == document.getElementById("esqueciemail").value) {
+                        senhacor = obj.senha;
+                        enviaremail = true;
+                    }
+                });
+                if (enviaremail) // criar um controller para enviar e-mail esqueci a senha 
+                    EnviarEmailEsqueci(document.getElementById("esqueciemail").value, senhacor);
+                   
+                else
+                    alert("Usuário não authenticado no sistema");
+
+            },
+            error: function (data, textSaudacoes) {
+                $("#resposta").text(data);
+            }
+        });
+
+    }
+};
+
+
+var ValidarSenha = function (valor) {
+    if (valor != "") {
+        if (valor != document.getElementById("senhacorretor").value)
+            document.getElementById("resenha").style.border = '1px solid #FF0000';
+        else
+            document.getElementById("resenha").style.border = '1px solid #000000';
+    }else 
+        document.getElementById("resenha").style.border = '1px solid #000000';
+
+};
+
+var EnviarEmailEsqueci = function (email,senha) {
+    
+    $.ajax({
+        url: 'Admin/EsqueceuEmail/?email='+email + '&senha='+senha,
+        type: 'post',
+        datatype: 'json',
+        cache: false,
+        method: 'get',
+        success: function (data, textSaudacoes) {
+            alert("email enviado com sucesso");
+        }
+    });
+};
+
+var ValidadorEmail = function (email) {
+    var enviaremail = false;
+    if (email != "") {
+
+        if (email.indexOf("@") == -1) {
+            document.getElementById("Email").style.border = '1px solid #FF0000';
+        } else {
+            document.getElementById("Email").style.border = '1px solid #000000';
+
+            $.ajax({
+                url: 'http://localhost:51744/api/corretor/?valor=' + document.getElementById("Email").value,
+                type: 'get',
+                datatype: 'json',
+                success: function (data, textSaudacoes) {
+
+                    $.each(data, function (i, obj) {
+                        if (obj.email == document.getElementById("Email").value) {
+                            enviaremail = true;
+                        }
+                    });
+                    if (enviaremail) {// criar um controller para enviar e-mail esqueci a senha 
+                        alert("Cadastrado no Banco de daods");
+                        document.getElementById("Email").value = "";
+                        document.getElementById("Email").focus();
+                    }
+                    
+
+                }
+            });
+        }
+
+    }
+    else {
+        document.getElementById("Email").style.border = '1px solid #000000';
+    }
+};
+
+var CadEmpreendimento = function () {
+
+    var dados = $("#FormEmpreendimento").serialize();
+    $.ajax({
+        url: 'Empreendimento',
+        type: 'post',
+        datatype: 'json',
+        cache: false,
+        method: 'post',
+        data: dados,
+        success: function (data, textSaudacoes) {
+            $("#resposta").text(data);
+            $("#Nome").val() = "";
+            $("#Endereco").val() = "";
+            $("#Numero").val() = "";
+            $("#Estado").val() = "";
+            $("#Cidade").val() = "";
+            
+        },
+        error: function (data, textSaudacoes) {
+            $("#resposta").text(data);
+        }
+    });
+
+};
+
+
+var SalvarImovel = function () {
+
+    var dados = $("#FormImovel").serialize();
+    $.ajax({
+        url: 'Imovel',
+        type: 'post',
+        datatype: 'json',
+        cache: false,
+        method: 'post',
+        data: dados,
+        success: function (data, textSaudacoes) {
+            if (data.indexOf("Erro!") == 0) {
+                $("#resposta").text(data);
+            } else {
+                $("#resposta").text(data);
+                document.getElementById("NomeImovel").value = "";
+                document.getElementById("Descricao").value = "";
+                document.getElementById("Endereco").value = "";
+                document.getElementById("Numero").value = "";
+                document.getElementById("Estado").value = "";
+                document.getElementById("Cidade").value = "";
+                document.getElementById("IdPorcetagem_ID").value = "";
+                document.getElementById("Preco").value = "";
+
+            }
+        },
+        error: function (data, textSaudacoes) {
+            $("#resposta").text(data);
+        }
+    });
+
+};
+
+var CadPorcentagem = function () {
+    var dados = $("#FormPorcentagem").serialize();
+
+    $.ajax({
+        url: 'Porcentagem',
+        type: 'post',
+        datatype: 'json',
+        cache: false,
+        method: 'post',
+        data: dados,
+        success: function (data, txtstatus) {
+            $("#Desconto").val() = "";
+            $("#resposta").text(data);
+        }, error: function (data, txtstatus) {
+            $("#resposta").text(data);
+        }
+    });
+};
+
+$(function () {
+    var selecao = "";
+    
+        $("#EmpreendimentoID").change(function () {
+            $.ajax({
+                url: 'http://localhost:51744/api/imovel/?valor=' + $("#EmpreendimentoID").val(),
+                type: 'get',
+                datatype: 'json',
+                cache: false,
+                method: 'get',
+                success: function (data, txtstatus) {
+                    selecao = "<select name='ImovelID' id='ImovelID'>";
+                    $.each(data, function (i, obj) {
+                        selecao += "<option value= " + obj.imovelID + ">" + obj.nomeImovel + "</option>";
+                    });
+                    selecao += "</select>";
+                    document.getElementById("txtimovel").innerHTML = selecao;
+
+                }, error: function (data, txtstatus) {
+
+                }
+            });
+        });
+     
+
+});
